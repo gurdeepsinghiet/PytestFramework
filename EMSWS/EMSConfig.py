5f525f49b477ad83ba505ec2a7aa9467de06c85c
Singh-EXTERNAL Gurdeep <10059648@gemalto.com>
2022-07-04 11:56:38 +0530

    done update config file

--- EMSWS/EMSConfig.py
+++ EMSWS/EMSConfig.py
@@ -8,24 +8,6 @@ chromepath="C://jgjgjhj"
 client_secret=""
 client_id="ems-scl-public-client"
 grant_type="password"
-#Json paths
-nameSpaceJsonPath='\\jsonFiles\\nameSpace.json'
-featureJsonPath='\\jsonFiles\\feature.json'
-productJsonPath='\\jsonFiles\\product.json'
-contactJsonPath='\\jsonFiles\\contact.json'
-customerJsonPath='\\jsonFiles\\customer.json'
-entitlementJsonPath='\\jsonFiles\\entitlement.json'
-productKeyJsonPath='\\jsonFiles\\productKey.json'
-activationJsonPath='\\jsonFiles\\activation.json'
-partialEntitlementJsonPath='\\jsonFiles\\partialEntitlement.json'
-
-regTokenJsonPathwithCust='\\jsonFiles\\registrationTokensWithCustomer.json'
-regTokenJsonPathwithoutCust='\\jsonFiles\\registrationTokensWithoutCustomer.json'
-accessTokenJsonPath='\\jsonFiles\\accessToken.json'
-emsDataFramePath='./EMSDataFrame/'
-emsReportPath='\\output\\'
-roleJsonPath='\\jsonFiles\\role.json'
-userJsonPath='\\jsonFiles\\user.json'
 #XML Files Paths
 regTokenXmlPathwithCust='\\xmlFiles\\registrationTokensWithCustomer.xml'
 regTokenXmlPathwithoutCust='\\xmlFiles\\registrationTokensWithoutCustomer.xml'
